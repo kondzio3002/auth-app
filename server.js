ba9f07b6ca270dda370f8805bc7f88c66c9ec854
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-08 22:33:01 +0100

    Add /auth/google/callback

--- server.js
+++ server.js
@@ -3,32 +3,10 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
-
-passport.use(new GoogleStrategy({
-  clientID: '747874984361-hmdojr15nhlegl184uooq787hv0m54so.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  done(null, profile);
-}));
-
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
-
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -38,9 +16,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission'}), 
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
 
 app.get('/user/logged', (req, res) => {
   res.render('logged');
