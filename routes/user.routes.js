63a11b95b2a4f13f14f2df15b7d1d7ea68e2bb74
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-08 23:31:34 +0100

    Add logout and {avatar, name} for logged

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+const isLogged = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/user/no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  res.render('logged', {
+    userName: req.user.displayName,
+    avatar: req.user.photos[0].value
+  });
+});
+
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('profileSettings');
 });
 
 router.get('/no-permission', (req, res) => {
